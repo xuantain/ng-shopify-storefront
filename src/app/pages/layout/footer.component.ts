8be087a128f587a00446014bb76776409a58985c
xuantain <xuantain@gmail.com>
2019-07-20 12:18:57 +0700

    Style : Home page

--- src/app/pages/layout/footer.component.ts
+++ src/app/pages/layout/footer.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'layout-footer',
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
+  styleUrls: ['./layout.scss']
 })
 export class FooterComponent {
   today: number = Date.now();
--- src/app/pages/layout/header.component.ts
+++ src/app/pages/layout/header.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'layout-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./layout.scss']
 })
 export class HeaderComponent {
   constructor() {}
